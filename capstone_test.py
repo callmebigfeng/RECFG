a92b66114c94e0d60118427451440e12dbd9c9b2
callmebigfeng <1269678415@qq.com>
2020-09-29 09:53:58 +0800

    test

--- capstone_test.py
+++ capstone_test.py
@@ -1,12 +1,23 @@
 from capstone import *
 
 CODE = b"\xc3"
+CODE2 =b"\x18\\U\x00\x18\\U\x00\x18\\U\x00\x18\\U\x00\x18\\U\x00\x18"
 md = Cs(CS_ARCH_X86, CS_MODE_32)
 count = 0
 for i in md.disasm(CODE, 0x1000):
     count += 1
     print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+print(CODE)
+if count > 1:
+    print("转换成多指令")
+else :
+    print("转换成单指令")
 print()
+count = 0
+for i in md.disasm(CODE2, 0x1000):
+    count += 1
+    print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+print(CODE2)
 if count > 1:
     print("转换成多指令")
 else :
