a362829a381246f72da6e531a04d1d16e2bb17ef
callmebigfeng <1269678415@qq.com>
2020-09-30 17:04:57 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pefile
+import pandas as pd
 from capstone import *
 path = "./PE/GifCam.exe"
 
@@ -24,41 +25,51 @@ for section in pe.sections:
             i = i + 1
         if section.SizeOfRawData == len(text):
             print('代码段存取成功!')
-        #print('c2出现次数：   %d' % text.count(b'\xc2'))
-        #print('c3出现次数：   %d' % text.count(b'\xc3'))
-        tmp = b''.join(text)
+            #print('c2出现次数：   %d' % text.count(b'\xc2'))
+            #print('c3出现次数：   %d' % text.count(b'\xc3'))
+            tmp = b''.join(text)
 
 
-        #编译整个.text代码节
-        #  问题  ：  遇到大量无法反汇编的字节，capstone不报错，自动停止，不知道这段字节长度
-        #  问题二  ：  capstone 和IDA　　完全对不上
-        md = Cs(CS_ARCH_X86, CS_MODE_32)
-        mnem = []     #存储反汇编后地址，指令，操作数，二进制，指令长度
-        offset = 0
-        print("代码段总长度： %d" %len(text))
-        while offset < len(text) :
-            for i in md.disasm(tmp[offset:], offset):
-                print("0x%x:\t%s\t%s\t%s\t%d" % (i.address, i.mnemonic, i.op_str,i.bytes,i.size))
-                mnem.append((i.address,i.mnemonic, i.op_str,i.bytes,i.size))
-            offset+=(mnem[-1][0]+mnem[-1][4])
+            #编译整个.text代码节
+            #  问题  ：  遇到大量无法反汇编的字节，capstone不报错，自动停止，不知道这段字节长度
+            #  问题二  ：  capstone 和IDA　　完全对不上
+            md = Cs(CS_ARCH_X86, CS_MODE_32)
+            mnem = []     #存储反汇编后地址，指令，操作数，二进制，指令长度
+            offset = 0
+            print("代码段总长度： %d" %len(text))
+            while offset < len(text) :
+                for i in md.disasm(tmp[offset:], offset):
+                    #print("0x%x:\t%s\t%s\t%s\t%d" % (i.address, i.mnemonic, i.op_str,i.bytes,i.size))
+                    mnem.append((i.address,i.mnemonic, i.op_str,i.bytes,i.size))
+                offset+=(mnem[-1][0]+mnem[-1][4])
 
-        #生成程序流程图
-        CFG = dict()  # CFG图
-        for i in range(len(mnem)) :
-            if mnem[i][1] == 'jmp' :         #  还有一些 ret，call之类的指令
-                if mnem[i][2][:2]==  '0x' :       # 跟着地址直接跳转
-                    for  j in range(len(mnem)) :
-                        if hex(mnem[j][0])== mnem[i][2]   :
-                            CFG[mnem[i][0], mnem[i][1], mnem[i][2], str(mnem[i][3])] = [(           #   jmp ->  address
-                            mnem[j][0], mnem[j][1], mnem[j][2], str(mnem[j][3]))]
-                else :
-                    pass          #  未完成 ：jmp  dword ptr   寄存器/地址   需要取出寄存器的值？
-            else:
-                if i < len(mnem)-1 :
-                    CFG[mnem[i][0],mnem[i][1],mnem[i][2],str(mnem[i][3])] = [(mnem[i+1][0],mnem[i+1][1],mnem[i+1][2],str(mnem[i+1][3]))]    # 将上一条指令指向下一条指令
-                if  mnem[i][1] == 'jz' :      #  还有其他跳转指令
-                    pass       #  代码同  jmp    将跳转位置添加到key中
-        #print(CFG)
+            #生成程序流程图
+            CFG = dict()  # CFG图
+            for i in range(len(mnem)) :
+                if mnem[i][1] == 'jmp' :         #  还有一些 ret，call之类的指令
+                    if mnem[i][2][:2]==  '0x' :       # 跟着地址直接跳转
+                        for  j in range(len(mnem)) :
+                            if hex(mnem[j][0])== mnem[i][2]   :
+                                CFG[mnem[i][0], mnem[i][1], mnem[i][2], str(mnem[i][3])] = [(           #   jmp ->  address   //从地址处开始反汇编 （8未完成）
+                                mnem[j][0], mnem[j][1], mnem[j][2], str(mnem[j][3]))]
+                            else:            #问题 ： 相同字节直接编译 和 有前后文编译有区别么 ？
+                                pass
+                    else :
+                        pass          #  未完成 ：jmp  dword ptr   寄存器/地址   需要取出寄存器的值？
+                else:
+                    if i < len(mnem)-1 :
+                        CFG[mnem[i][0],mnem[i][1],mnem[i][2],str(mnem[i][3])] = [(mnem[i+1][0],mnem[i+1][1],mnem[i+1][2],str(mnem[i+1][3]))]    # 将上一条指令指向下一条指令
+                    if  mnem[i][1] == 'jz' :      #  还有其他跳转指令
+                        pass       #  代码同  jmp    将跳转位置添加到key中
+            #print(CFG)
+
+            #程序流程图  字典 ——> 邻接矩阵
+            CFG = {k: [v for v in vs] for k, vs in CFG.items()}
+            edges = [(a, b) for a, bs in CFG.items() for b in bs]
+            df = pd.DataFrame(edges)
+            adj_matrix = pd.crosstab(df[0], df[1])
+            print(adj_matrix)
+
 
 
 
