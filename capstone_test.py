3acd59d901d56b8e23b85122ec3870df2e0595ba
callmebigfeng <1269678415@qq.com>
2020-10-06 20:01:40 +0800

    update

--- capstone_test.py
+++ capstone_test.py
@@ -1,14 +1,24 @@
 from capstone import *
 
 CODE = b"\11"
-CODE2 =b"\x18\\U\x00\x18\\U\x00\x18\\U\x00\x18\\U\x00\x18\\U\x00\x18"
+CODE2 =b"\00"
 md = Cs(CS_ARCH_X86, CS_MODE_32)
+md.detail = True
 count = 0
-disa = md.disasm(CODE, 0x1000)
+disa = md.disasm(CODE2, 0x1f)
 print(CODE)
 for i in disa:
     count += 1
-    print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+    print("0x%x:\t%s\t%s\t%s" % (i.address, i.mnemonic, i.op_str,i.reg_read))
+    print(i.regs_access)
+    if len(i.regs_read) > 0:
+        for r in i.regs_read :
+            print("%s " % i.reg_name(r)),
+        print()
+    if len(i.groups) > 0:
+        for g in i.groups:
+            print("%u" %g)
+        print()
 if count==0:
     print("无指令")
 elif count > 1:
@@ -17,3 +27,6 @@ else :
     print("转换成单指令")
 
 # 问题 机器码既能转换成单指令又能转换成多指令
+# code = b'\x04\x10\x40\x00'
+# asm = cs_disasm_quick(CS_ARCH_X86, CS_MODE_32,code,0x100,count=1)
+# print(asm)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pefile
 import pandas as pd
 from capstone import *
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
 path = "./PE/GifCam.exe"
 
 pe = pefile.PE(path)
@@ -32,45 +36,75 @@ for section in pe.sections:
 
             #编译整个.text代码节
             #  问题  ：  遇到大量无法反汇编的字节，capstone不报错，自动停止，不知道这段字节长度
-            #  问题二  ：  capstone 和IDA　　完全对不上
+            #  问题二  ：  capstone 和IDA　　完全对不上   --> 部分数据被强制反汇编
             md = Cs(CS_ARCH_X86, CS_MODE_32)
             mnem = []     #存储反汇编后地址，指令，操作数，二进制，指令长度
             offset = 0
-            print("代码段总长度： %d" %len(text))
+            offset2 = 0
+            print("代码段总长度： %s" %hex(len(text)))
             while offset < len(text) :
                 for i in md.disasm(tmp[offset:], offset):
                     #print("0x%x:\t%s\t%s\t%s\t%d" % (i.address, i.mnemonic, i.op_str,i.bytes,i.size))
                     mnem.append((i.address,i.mnemonic, i.op_str,i.bytes,i.size))
                 offset+=(mnem[-1][0]+mnem[-1][4])
 
+
+
             #生成程序流程图
+            #  jz 和  je
+            #  capstone 将数据字节强行反汇编，其他部分反汇编结果正确
+            #  jmp跳出段外 ，下一段的位置从哪开始
+            #  不断遇到jmp不断跳转
             CFG = dict()  # CFG图
             for i in range(len(mnem)) :
-                if mnem[i][1] == 'jmp' :         #  还有一些 ret，call之类的指令
+                if mnem[i][1] == 'jmp' :    #  还有一些 ret，call之类的指令
+                    #print(hex(mnem[i][0]), mnem[i][1], mnem[i][2], str(mnem[i][3]))
                     if mnem[i][2][:2]==  '0x' :       # 跟着地址直接跳转
                         for  j in range(len(mnem)) :
                             if hex(mnem[j][0])== mnem[i][2]   :
                                 CFG[mnem[i][0], mnem[i][1], mnem[i][2], str(mnem[i][3])] = [(           #   jmp ->  address   //从地址处开始反汇编 （未完成）
-                                mnem[j][0], mnem[j][1], mnem[j][2], str(mnem[j][3]))]
-                            else:            #问题 ： 相同字节直接编译 和 有前后文编译有区别么 ？
+                                mnem[j][0], mnem[j][1], mnem[j][2], str(mnem[j][3]))]                      #直接反汇编再处理跳转 ，jmp会不会跳到指令中间
+                            else:
                                 pass
-
                     else :
                         pass          #  未完成 ：jmp  dword ptr   寄存器/地址   需要取出寄存器的值？
+                if mnem[i][1] == 'call' :
+                    pass
                 else:
                     if i < len(mnem)-1 :
                         CFG[mnem[i][0],mnem[i][1],mnem[i][2],str(mnem[i][3])] = [(mnem[i+1][0],mnem[i+1][1],mnem[i+1][2],str(mnem[i+1][3]))]    # 将上一条指令指向下一条指令
                     if  mnem[i][1] == 'jz' :      #  还有其他跳转指令
                         pass       #  代码同  jmp    将跳转位置添加到key中
-            #print(CFG)
+            print(CFG)
+
 
             #程序流程图  字典 ——> 邻接矩阵
             CFG = {k: [v for v in vs] for k, vs in CFG.items()}
             edges = [(a, b) for a, bs in CFG.items() for b in bs]
+            tmp = [(a,a) for a in CFG.keys()]
+            total_value = []
+            for vs in CFG.values():
+                total_value += vs
+            tmp2 = [(v,v) for v in total_value]
+
+            edges += tmp + tmp2
+            edges = list(set(edges))
+
             df = pd.DataFrame(edges)
-            adj_matrix = pd.crosstab(df[0], df[1])
-            print(adj_matrix)
+            matrix = pd.crosstab(df[0], df[1])
+            print(matrix)
 
 
+            #将图的邻接矩阵转化成图
+            G = nx.Graph()
+            #size = len(matrix)
+            size = 100
+            for i in range(size):
+                for j in range(size):
+                    if i!=j and matrix.iloc[i][j] == 1:
+                        G.add_edge(i, j)
+
+            nx.draw(G)
+            plt.show()
 
 
